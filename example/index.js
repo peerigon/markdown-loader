cfd4a63b13d6d522ccd54f57d0d17df75cd59a6b
yuntao.qyt <yuntao.qyt@alibaba-inc.com>
2016-01-28 19:27:07 +0800

    add example

--- example/index.js
+++ example/index.js
@@ -1,8 +1,4 @@
-// var html = JSON.parse(require("html!markdown!./README.md"));
-
-
 var html = require("./README.md");
 var container = document.getElementById('container');
 
 container.innerHTML = html;
-console.log(html)
