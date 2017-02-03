ab98d19e794a8f74295c14cbb12bc81ab65df99c
meaku <michael.jaser@peerigon.com>
2017-02-03 18:18:30 +0100

    Remove marked default options

--- index.js
+++ index.js
@@ -3,20 +3,9 @@
 const marked = require("marked");
 const loaderUtils = require("loader-utils");
 
-const DEFAULT_OPTIONS = {
-    renderer: new marked.Renderer(),
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: true,
-    smartLists: true,
-    smartypants: false
-};
-
 module.exports = function (markdown) {
     // merge params and default config
-    const options = Object.assign(DEFAULT_OPTIONS, loaderUtils.parseQuery(this.query));
+    const options = loaderUtils.parseQuery(this.query);
 
     this.cacheable();
 
