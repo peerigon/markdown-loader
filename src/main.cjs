96b75a27aca9e4fe24aec304b57d52facc89800c
Johannes Ewald <johannes.ewald@peerigon.com>
2022-01-27 18:33:29 +0100

    Add comment about trampolin module

--- src/main.cjs
+++ src/main.cjs
@@ -1,3 +1,5 @@
+// This file is currently necessary because webpack needs the loader to be a CommonJS module.
+// Its only purpose is to serve as a "trampolin" for the actual loader module.
 /* eslint-disable @babel/no-invalid-this */
 "use strict";
 
